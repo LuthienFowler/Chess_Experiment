8dea24668228a9ddd9c9a3f418fe7a14b1dfbf0a
Luth <noafowl@gmail.com>
2024-08-06 22:02:44 -0600

    Created the tile

---
+++ Tile.gd
@@ -0,0 +1,21 @@
+extends ColorRect
+
+## Variables #######################################################################################
+
+@export var dark = false
+
+## Default functions ###############################################################################
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func color_tile():
+	if dark:
+		pass
