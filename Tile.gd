5a9653f6f14c6370fa9965e50fcd7d9d5ecc3b9d
Luth <noafowl@gmail.com>
2024-08-07 09:50:14 -0600

    Naming the pieces...
    This was probably the worst way to make the chess board but oh well

--- Tile.gd
+++ Tile.gd
@@ -3,6 +3,7 @@ extends ColorRect
 ## Variables #######################################################################################
 
 @export var dark = false
+@export var location = " "
 
 ## Default functions ###############################################################################
 
@@ -19,3 +20,7 @@ func color_tile():
 		color = Color("380e02")
 	else:
 		color = Color("eda64a")
+
+
+func _on_button_pressed():
+	print(location + " has been pressed!")
