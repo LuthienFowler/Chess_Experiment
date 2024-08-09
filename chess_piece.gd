71c3e0ad8d25635eadbde25420b7d6d693ecec30
Luth <noafowl@gmail.com>
2024-08-08 21:05:19 -0600

    Made the chess pieces visible and (almost) running

--- chess_piece.gd
+++ chess_piece.gd
@@ -1,20 +1,41 @@
 extends Node2D
 
 enum piece {KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN}
+enum color {BLACK, WHITE}
 
 ## Variables #######################################################################################
 
 @export var piece_type = piece.PAWN
+@export var piece_color = color.BLACK
+
+@onready var color_rect = $ColorRect
+@onready var label = $Label
 
 ## Default functions ###############################################################################
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if piece_color == color.BLACK:
+		color_rect.color = Color("000214")
+		label.set("theme_override_colors/font_color", Color("b6bed1"))
+	else:
+		color_rect.color = Color("b6bed1")
+		label.set("theme_override_colors/font_color", Color("000214"))
+	
+	match piece_type:
+		piece.KING:
+			label.text = "King"
+		piece.QUEEN:
+			label.text = "Queen"
+		piece.ROOK:
+			label.text = "Rook"
+		piece.BISHOP:
+			label.text = "Bishop"
+		piece.KNIGHT:
+			label.text = "Knight"
+		piece.PAWN:
+			label.text = "Pawn"
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 
