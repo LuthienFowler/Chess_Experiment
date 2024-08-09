a6eac2524a6275a76409a8d77f4e6968fb1e5e94
Luth <noafowl@gmail.com>
2024-08-08 20:29:42 -0600

    Made the chess piece scene
    Yayy finally at the pieces

---
+++ chess_piece.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+enum piece {KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN}
+
+## Variables #######################################################################################
+
+@export var piece_type = piece.PAWN
+
+## Default functions ###############################################################################
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+## Functions #######################################################################################
